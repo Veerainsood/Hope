56055c34ad487332014d33bfc5368084e6faa982
veerain <veerainsood1@gmail.com>
2024-08-05 17:56:42 +0530

    Temp

--- app/src/main/java/com/example/hopedonationapp/MainActivity.kt
+++ app/src/main/java/com/example/hopedonationapp/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.hopedonationapp
 
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.hopedonationapp.R
+import android.widget.VideoView
 import android.os.Bundle
-import android.widget.Button
+import android.net.Uri;
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() { // Activity == Screens
     // App Compact is used for backward compatibility + loading of dependencies,,,,,
@@ -21,7 +17,6 @@ class MainActivity : AppCompatActivity() { // Activity == Screens
         // inflates the layout , creates connection between layout and Main Acitvity
         // R class is for recourse...   R.layout.activity_main -> conneects Main with xml representing layout elements
         //without this everything is 0/100
-
     }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
